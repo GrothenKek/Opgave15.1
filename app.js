19a628db48a2816fcbc375e882fb0d9890479b50
GrothenKek <65117443+GrothenKek@users.noreply.github.com>
2020-10-29 12:13:33 +0100

    yea

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const config = require('./config');
 
-
+// TODO slet 
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use('/rooms', require('./routes/rooms.js'));
