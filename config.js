ea7498f3c254f9efab449675281b3a60f31704c4
GrothenKek <65117443+GrothenKek@users.noreply.github.com>
2020-11-03 09:03:20 +0100

    k

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // config.js
 const config = {
-    databaseURI: 'mongodb+srv://Grothen:<p4ndek4gek0ngen>@jokeservice.0lfnc.mongodb.net/<chatserver>?retryWrites=true&w=majority' //'mongodb://localhost' + '/chatserver', // or MongoDB Atlas connection URI
+    databaseURI: 'mongodb'+'srv://Grothen:<p4ndek4gek0ngen>@jokeservice.0lfnc.mongodb.net/<chatserver>?retryWrites=true&w=majority', //'mongodb://localhost' + '/chatserver', // or MongoDB Atlas connection URI
+    localport : 8080
 };
 
 module.exports = config;
